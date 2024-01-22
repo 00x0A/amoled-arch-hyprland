1e685e887357e5409fe4f808fbf241ff3519ab17
Isak <00x0a@proton.me>
2024-01-22 20:10:52 +0100

    added more fish shortcuts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,9 @@
 set fish_greeting
 starship init fish | source
 
-alias gh-add="git add ."
+alias gh-apply="git add ."
 alias gh-commit="git commit -m "
 alias gh-push="git push -u origin "
 alias gh-pull="git pull"
+alias gh-create="gh repo create"
 
